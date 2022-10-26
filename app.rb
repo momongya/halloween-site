f69bf3f054482985dc38caf5baf8326ecdbf0177
EC2 Default User <ec2-user@ip-10-1-8-158.ap-northeast-1.compute.internal>
2022-10-26 14:28:02 +0000

    first commit

--- app.rb
+++ app.rb
@@ -4,5 +4,28 @@ require 'sinatra/reloader' if development?
 require './models.rb'
 
 get '/' do
+    
   erb :index
+end
+
+post '/zibu' do
+    @zibu = "zibu"
+#   debug.log("test OK")
+end
+
+get '/api/game' do
+    uri = URI("http://express.heartrails.com/api/json")
+    uri.query = URI.encode_www_form({
+        name: params[:name]
+    })
+    res = Net::HTTP.get_response(uri)
+    json = JSON.parse(res.body)
+    if json["response"]["error"]
+        response ={ error: "No Station." }
+    else
+        response = {
+            next: json["response"]
+        }
+    end
+    json response
 end
