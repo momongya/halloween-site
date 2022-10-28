45a205c379aa0ed1d1767e7fb5e086cfdeb4bb6d
momongya <mo5e2ka8@gmail.com>
2022-10-28 07:50:37 +0000

    大体完成

--- public/js/font.js
+++ public/js/font.js
@@ -3,9 +3,8 @@ function resize() {
   $resizeText.each(function() {
     var wrapper = $(this).width() / $('body').width();
     var fontSizeVw = wrapper / $(this).text().length;
-    // ▼ letter-spacingを0.1emに指定していた場合、1.1をかける
     // var fontSizeVw = wrapper / ($(this).text().length * 1.1);
-    $(this).css('font-size', fontSizeVw * 100 + 'vw');
+    $(this).css('font-size', fontSizeVw * 140 + 'vw');
   });
 }
 // 初期
